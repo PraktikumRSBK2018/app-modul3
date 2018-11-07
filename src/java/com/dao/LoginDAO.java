f5e509c762cfdec240e5197737a63b08ace5ba6d
haritsf <33232330+haritsf@users.noreply.github.com>
2018-11-07 09:39:09 +0700

    Add files via upload

---
+++ src/java/com/dao/LoginDAO.java
@@ -0,0 +1,26 @@
+package com.dao; 
+import com.model.Login; 
+import java.util.List; 
+import javax.ejb.Stateless; 
+import javax.persistence.EntityManager; 
+import javax.persistence.PersistenceContext; 
+/** 
+* 
+* @author Rosita Permata Dewi 
+*/ 
+@Stateless 
+public class LoginDAO implements LoginDAOLocal { 
+@PersistenceContext 
+private EntityManager em; 
+@Override 
+public boolean checkUser(String userName, String password) { 
+List<Login> s = (List<Login>)em.createQuery("select e from Login e where e.userName='"+userName+"' and e.password='"+password+"'").getResultList(); 
+System.out.println("is list empty ?"+s.isEmpty()+" for the"+userName+" and "+password);
+if(!s.isEmpty()) 
+return true; 
+else 
+return false; 
+} 
+// Add business logic below. (Right-click in editor and choose 
+// "Insert Code > Add Business Method") 
+}
